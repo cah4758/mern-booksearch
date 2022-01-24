eec1541650a0944947212ad40bebe595a3819d93
Charles Hernandez <charlesh4758@gmail.com>
2022-01-23 19:17:49 -0600

    Update auth middleware to connect graphql

--- server/server.js
+++ server/server.js
@@ -11,8 +11,8 @@ const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers,
-  context: authMiddleware,
+  // resolvers,
+  // context: authMiddleware,
 });
 
 server.applyMiddleware({ app });
@@ -23,11 +23,10 @@ app.use(express.json());
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
 }
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
 
 db.once("open", () => {
   app.listen(PORT, () => {
